d674d99433fcc26952083fad0d7dc9abb5cbeb69
Saleumsack <saleumsack16@gmail.com>
2018-10-22 00:15:51 +0700

    ັAdd Laos language

---
+++ components/locale-provider/lo_LO.tsx
@@ -0,0 +1,44 @@
+import Pagination from 'rc-pagination/lib/locale/lo_LO';
+import DatePicker from '../date-picker/locale/lo_LO';
+import TimePicker from '../time-picker/locale/lo_LO';
+import Calendar from '../calendar/locale/lo_LO';
+
+export default {
+  locale: 'lo',
+  Pagination,
+  DatePicker,
+  TimePicker,
+  Calendar,
+  Table: {
+    filterTitle: 'ໂຕກອງ',
+    filterConfirm: 'ຢືນຢັນ',
+    filterReset: 'ລີເຊັດ',
+    emptyText: 'ບໍ່ມີຂໍ້ມູນ',
+    selectAll: 'ເລືອກທັງໝົດໃນໜ້ານີ້',
+    selectInvert: 'ເລືອກສະຖານະກົງກັນຂ້າມ',
+  },
+  Modal: {
+    okText: 'ຕົກລົງ',
+    cancelText: 'ຍົກເລີກ',
+    justOkText: 'ຕົກລົງ',
+  },
+  Popconfirm: {
+    okText: 'ຕົກລົງ',
+    cancelText: 'ຍົກເລີກ',
+  },
+  Transfer: {
+    notFoundContent: 'ບໍ່ພົບຂໍ້ມູນ',
+    searchPlaceholder: 'ຄົ້ນຫາ',
+    itemUnit: 'ລາຍການ',
+    itemsUnit: 'ລາຍການ',
+  },
+  Select: {
+    notFoundContent: 'ບໍ່ພົບຂໍ້ມູນ',
+  },
+  Upload: {
+    uploading: 'ກຳລັງອັບໂຫຼດ...',
+    removeFile: 'ລຶບຟາຍ',
+    uploadError: 'ເກີດຂໍ້ຜິດພາດໃນການອັບໂຫຼດ',
+    previewFile: 'ເບິ່ງໂຕຢ່າງຟາຍ',
+  },
+};
